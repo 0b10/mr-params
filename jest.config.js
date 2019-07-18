3c12f6b768d28bf53624f8bdcaa325d363d43ae4
0b10 <0b10@pm.me>
2019-07-18 21:54:51 +0100

    Move tests to TS

--- jest.config.js
+++ jest.config.js
@@ -141,7 +141,7 @@ module.exports = {
   testMatch: [
     "**/__tests__/**/*.[jt]s?(x)"
     // "**/?(*.)+(spec|test).[tj]s?(x)"
-  ]
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
@@ -165,6 +165,9 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   // transform: null,
+  transform: {
+    ".(ts|tsx)": "ts-jest"
+  }, 
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
